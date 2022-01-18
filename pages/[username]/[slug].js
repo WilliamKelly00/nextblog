6b11639b928f409ce0c1890fa8f96aed9bd2ef94
WilliamKelly00 <williammkelly00@gmail.com>
2022-01-18 03:11:22 -0500

    post page added

--- pages/[username]/[slug].js
+++ pages/[username]/[slug].js
@@ -1,3 +1,45 @@
+import styles from '../../styles/Post.module.css';
+import PostContent from '../../components/PostContent';
+import { firestore, getUserWithUsername, postToJSON } from '../../lib/firebase';
+import { useDocumentData } from 'react-firebase-hooks/firestore';
+
+
+export async function getStaticProps({ params }){
+    const {username, slug} = params;
+    const userDoc = await getUserWithUsername(username);
+    
+    let post;
+    let path;
+
+    if(userDoc){
+        const postRef = userDoc.ref.collection('posts').doc(slug);
+        post = postToJSON(await postRef.get());
+
+        path = postRef.path;
+    }
+
+    return{
+        props: {post, path},
+        revalidate: 5000,
+    };
+}
+
+export async function getStaticPaths(){
+    const snapshot = await firestore.collection('posts').get();
+    
+    const paths = snapshot.docs.map(doc => {
+        const { slug, username } = doc.data();
+        return {
+            params: { username, slug },
+        };
+    });
+    
+    return{
+        paths,
+        fallback: 'blocking',
+    };
+}
+
 export default function PostPage({}){
     return (
         <main>
