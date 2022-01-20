58ba683180f2f882566ab00496cc0e8331b7e3f7
WilliamKelly00 <williammkelly00@gmail.com>
2022-01-20 12:44:40 -0500

    Added component to check authorization

---
+++ components/AuthCheck.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import { useContext } from "react";
+import { UserContext } from "../lib/context";
+
+export default function AuthCheck({ children, user, ...props }) {
+    const {username} = useContext(UserContext);
+
+    return username ?
+        props.children :
+        props.fallback || <Link href="/enter">You must be signed in</Link>;
+
+
+}
---
+++ pages/admin/index.js
@@ -0,0 +1,11 @@
+import AuthCheck from "../../components/AuthCheck";
+
+export default function AdminPostsPage({}){
+    return(
+        <main>
+            <AuthCheck>
+
+            </AuthCheck>
+        </main>
+    )
+}
